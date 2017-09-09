a208d9e4273cfda9806f18adcf33e7d23c759bd1
Daniel Thompson <danthwa@gmail.com>
2017-09-09 14:37:16 -0700

    initialized ios react project

---
+++ RavenV2/index.ios.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Roost extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      ravens: props.count
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Raven></Raven>
+        <Text>The Roost</Text>
+      </View>
+      
+    );
+    
+  }
+}
+
+export default class RavenV2 extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+         <Roost /> 
+        <Text style={styles.welcome}>
+          Welcome to React Native! WE HERE!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('RavenV2', () => RavenV2);
