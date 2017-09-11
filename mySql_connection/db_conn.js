134a8a25beb5d681782ce31851923f1ac20f0d4e
Steven Huggard <stevenhuggard@15MBP.local>
2017-09-11 06:30:52 -0700

    more query fncs

--- mySql_connection/db_conn.js
+++ mySql_connection/db_conn.js
@@ -1,20 +1,50 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "raven-messages"
-});
+class SQL_Connection {
+  constructor(){
+    this.con = this.get_connection()
+  }
 
+  get_connection() { 
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "raven-messages"
+    })
+    connection.connect(function(err) 
+      {if (err) throw err;})
+    return connection;
+  }
 
-_get_users = "SELECT * FROM users"
+  ex_query(_qry){
+    this.con.query(_qry, function(err, result, fields) {
+      if(err) throw err;
+      console.log("return: ", result);
+    })
+  }
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query(_get_users, function(err, result, fields) {
-    if(err) throw err;
-    console.log("return: ", result);
-  })
-});
+  select_all(table) {
+    let _qry = "SELECT * FROM " + table;
+    ex_query(_qry);
+  }
+
+  add_user(userName, password, email) {
+    let _qry = "INSERT INTO users (username, password, email, created_at," +
+               `updated_at) values ('${userName}', '${password}', '${email}'` +
+               ", now(), now())";
+    ex_query(_qry);
+  }
+
+  save_message(userID, msgText) {
+    let _qry = "INSERT INTO messages (text, user_id, created_at, updated_at)" +
+               `values ('${msgText}', '${userID}', now(), now())`;
+    ex_query(_qry);
+  }
+
+}
+
+connx = new SQL_Connection()
+connx.select_all("users")
+//connx.add_user("steven", "password", "steven@mail.com")
 
